7e78149db4407c8985621c32722c18606754c78a
Dingbang Chen <chen.dingb@northeastern.edu>
2024-04-10 23:46:59 -0400

    header button

--- client/src/components/fakestackoverflow.js
+++ client/src/components/fakestackoverflow.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./header";
 import Main from "./main";
+import {useState} from "react";
 
 export default function fakeStackOverflow() {
+    const [search, setSearch] = useState("");
+    const [page, setPage] = useState("home");
+    const setMainPage = (search = "", page= "home") => {
+        setSearch(search);
+        setPage(page);
+    };
     return (
         <>
-            <Header />
-            <Main/>
+            <Header search = {search} setMainPage = {setMainPage}/>
+            <Main search={search} passpage = {page}/>
         </>
     );
 }
