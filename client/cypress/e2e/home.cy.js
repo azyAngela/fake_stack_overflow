0b70ca4d4a934269c992af6026229ad4070f42a5
Dingbang Chen <chen.dingb@northeastern.edu>
2024-04-15 23:55:58 -0400

    init data

--- client/cypress/e2e/home.cy.js
+++ client/cypress/e2e/home.cy.js
@@ -2,4 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('renders questions list',()=>{
+    cy.visit("http://localhost:3000");
+    cy.contains("Create New Post").click();
+  });
 })
